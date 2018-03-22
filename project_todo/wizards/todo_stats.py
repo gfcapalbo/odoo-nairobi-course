ffa4b9bdd31dcfb5aeaecec4b4580fce29f7b9b3
Giovanni Francesco Capalbo <giovanni@therp.nl>
2018-03-22 11:01:14 +0300

    [ADD] wizards views buttons and functions

--- project_todo/wizards/todo_stats.py
+++ project_todo/wizards/todo_stats.py
@@ -11,14 +11,34 @@ from odoo import models
 # how many created by him
 # how many created that where given to him
 # How many modified by him
-#
+
+
+
 class ProjectTodoStats(models.TransientModel):
     
     _name = "project.todo.stats"
     _description = " statistics for project todo"
 
-    user_id = fields.date()
+    user_id = fields.Date()
     date = fields.Date()
 
 
+    def launch_stats(self):
+        """
+        functions can return actions, it's a valid alternative 
+        to using menues to launch actions. and can be used to implement 
+        multi popup types of situations (your context will follow you around)
+        """
+        return {
+                'type': 'ir.actions.act_window',
+                'res_model':  'project.todo',
+                'view_type': 'form',
+                'view_mode': 'form',
+                'target': 'new',
+                "view_id": self.env.ref('project_todo.form_todos'),
+                "views": [(self.env.ref('project_todo.form_todos'), 'form')]
+        }
+
+
+    def do_stats(self):
 
