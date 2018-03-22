2e057183e397849ec1694413ab441f56903038f4
Giovanni Francesco Capalbo <giovanni@therp.nl>
2018-03-22 12:34:33 +0300

    [ADD] extended wizard.

---
+++ project_todo/wizards/todo.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+from odoo import api, models, fields
+
+
+
+# wizard that given date and user, calculates the stats for that user
+
+# how many done 
+# how many created by him
+# how many created that where given to him
+# How many modified by him
+
+
+
+class ProjectTodo(models.Model):
+
+    _inherit = 'project.todo'
+    """
+    EXERCISE TRANSIENT to Model  Try making thi transient and see the mess that happens
+    """
+
+    """Note we are appling inheritance on our own model.
+    in order to have the definition available, verify that the import
+    of this wizard happens AFTER the import of the model it inherits in the imports 
+    of the module.
+    
+    We do this for two reasons
+        a) the button launch stats is on a form of project.todo. so the function it calls must
+        be of this type.
+        b) we do not put it in our project.todo class in /models   
+        directly to keep a orderly separation between 
+        wizard functionality and non-wizard functionality. this is just a style choice.
+    """
+
+
+
+
+    @api.multi
+    def launch_stats(self):
+        import pudb
+        pudb.set_trace()
+        self.ensure_one()
+        """
+        functions can return actions, it's a valid alternative 
+        to using menues to launch actions. and can be used to implement 
+        multi popup types of situations (your context will follow you around)
+        """
+        return {
+                'type': 'ir.actions.act_window',
+                'res_model':  'project.todo.stats',
+                'view_type': 'form',
+                'view_mode': 'form',
+                'target': 'new',
+        }
+
--- project_todo/wizards/todo_stats.py
+++ project_todo/wizards/todo_stats.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import datetime
-from odoo import models, fields
+from odoo import api, models, fields
 
 
 
@@ -20,7 +20,7 @@ class ProjectTodoStats(models.TransientModel):
     _description = " statistics for project todo"
 
     user_id = fields.Many2one('res.users', string='Stats Of')
-    date = fields.Date(string='After Date')
+    from_date = fields.Date(string='After Date')
 
 
     def launch_stats(self):
@@ -29,6 +29,8 @@ class ProjectTodoStats(models.TransientModel):
         to using menues to launch actions. and can be used to implement 
         multi popup types of situations (your context will follow you around)
         """
+        import pudb
+        pudb.set_trace()
         return {
                 'type': 'ir.actions.act_window',
                 'res_model':  'project.todo',
@@ -40,6 +42,23 @@ class ProjectTodoStats(models.TransientModel):
         }
 
 
+    """ Another inheritance example
+    we extend the default_get from odoo core models 
+    """
+    @api.model
+    def default_get(self, fields_list):
+        import pudb
+        pudb.set_trace()
+        result = super(ProjectTodoStats, self).default_get(fields_list=fields_list)
+        project_todo_stat_model = self.env['project.todo.stats']
+        current_stat_obj = project_todo_stat_model.browse(self.env.context['params']['id'])
+        result['user_id'] = current_stat_obj.user_id
+        result['from_date'] = current_stat_obj.from_date
+        return result
+
+
     def do_stats(self):
+        import pudb
+        pudb.set_trace()
         return True
 
